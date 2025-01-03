57dea13d7e580287e94750ead8d12c898e9d8d13
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-01-03 15:40:36 +0100

    add read-eval-print-loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
 //         Uncomment this block to pass the first stage
-         System.out.print("$ ");
+        System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
 
-        System.out.println(input + ": command not found\n");
+        while (scanner.hasNextLine()) {
+           String input = scanner.nextLine();
+           System.out.print(input + ": command not found\n");
+           System.out.print("$ ");
+        }
+
     }
 }
