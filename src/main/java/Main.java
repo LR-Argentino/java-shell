87fb5329621061bcc1e4c6dae76d8671caa00ee7
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-01-05 17:02:45 +0100

    add exit command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
 
+enum Commands {
+    EXIT("exit 0");
+
+    public final String label;
+
+    private Commands(String label) {
+        this.label = label;
+    }
+}
+
 public class Main {
     public static void main(String[] args) throws Exception {
 //         Uncomment this block to pass the first stage
@@ -9,6 +19,11 @@ public class Main {
 
         while (scanner.hasNextLine()) {
            String input = scanner.nextLine();
+
+           if (input.equals(Commands.EXIT.label)) {
+                break;
+           }
+
            System.out.print(input + ": command not found\n");
            System.out.print("$ ");
         }
